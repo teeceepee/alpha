cc57717f3fe49561a88a8730cc02469d40f9a91d
Yu Haidong <yuhaidongyhd@gmail.com>
2020-04-08 22:15:36 +0800

    Add comments

--- src/tree/binary-search-tree.ts
+++ src/tree/binary-search-tree.ts
@@ -1,3 +1,4 @@
+import { max } from '../utils'
 import { Comparator } from './comparator'
 
 class TreeNode<K, V> {
@@ -63,7 +64,7 @@ export class BinarySearchTree<K, V> {
     const leftDepth = this.depthRecursively(node.left)
     const rightDepth = this.depthRecursively(node.right)
 
-    return 1 + Math.max(leftDepth, rightDepth)
+    return 1 + max(leftDepth, rightDepth)
   }
 
   private getRecursively (node: TreeNode<K, V> | null, k: K): V | null {
@@ -117,6 +118,19 @@ export class BinarySearchTree<K, V> {
     }
   }
 
+  /**
+   * 删除节点
+   *
+   * 四种情况：
+   * 1. 如果节点为空，直接返回
+   * 2. 如果要删除当前节点，则删除并取到右子树的最小节点，以该节点为新的根节点
+   *    （也可以删除并取到左子树的最大节点，以该节点为新的根节点）
+   * 3. 如果要删除的节点在左子树，递归处理左子树
+   * 4. 如果要删除的节点在右子树，递归处理右子树
+   *
+   * @param node
+   * @param k
+   */
   private removeRecursively (node: TreeNode<K, V> | null, k: K): RemoveResult<K, V> {
     if (node == null) {
       return {newRoot: null, removedNode: null}
@@ -125,12 +139,13 @@ export class BinarySearchTree<K, V> {
     const compareResult: number = this.comparator(k, node.key)
 
     if (compareResult === 0) {
+      // 删除当前节点
       const removeResult = this.removeMinRecursively(node.right)
       const newNode = removeResult.removedNode
       const newRight = removeResult.newRoot
 
       let newRoot
-      if (newNode == null) {
+      if (newNode == null) { // 如果取到的是空，表示右子树为空，则删除节点后的新树就是左子树
         newRoot = node.left
       } else {
         newNode.left = node.left
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+
+export function max (n1: number, n2: number): number {
+  return Math.max(n1, n2)
+}
